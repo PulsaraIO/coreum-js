faa6772482b2e40fe4bd4d8a2faf88bee8b0e689
Alberto Robles <jalberto.oaks@gmail.com>
2023-05-07 21:31:52 -0700

    fix: added export for ft util method

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,15 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
+### [2.5.1](https://github.com/StreiaDF/coreum-js/compare/v2.5.0...v2.5.1) (2023-05-08)
+
+## [2.5.0](https://github.com/StreiaDF/coreum-js/compare/v2.4.1...v2.5.0) (2023-05-08)
+
+
+### Features
+
+* Added possible Leap support AND added util function parseTokenFeatures ([398b211](https://github.com/StreiaDF/coreum-js/commit/398b211027f321b960ecad585d6590b501551396))
+
 ### [2.4.1](https://github.com/StreiaDF/coreum-js/compare/v2.4.0...v2.4.1) (2023-05-08)
 
 ## 2.4.0 (2023-05-04)
--- dist/main/utils/index.js
+++ dist/main/utils/index.js
@@ -17,4 +17,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./wallet"), exports);
 __exportStar(require("./calculations"), exports);
 __exportStar(require("./nft"), exports);
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi9zcmMvdXRpbHMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7Ozs7OztBQUFBLDJDQUF5QjtBQUN6QixpREFBK0I7QUFDL0Isd0NBQXNCIn0=
+__exportStar(require("./ft"), exports);
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi9zcmMvdXRpbHMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7Ozs7OztBQUFBLDJDQUF5QjtBQUN6QixpREFBK0I7QUFDL0Isd0NBQXNCO0FBQ3RCLHVDQUFxQiJ9

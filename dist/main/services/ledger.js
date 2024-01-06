cca948b17588f47c54552b4158292212ea80e2cd
Alberto Robles <jalberto.oaks@gmail.com>
2024-01-05 20:39:48 -0600

    logs

--- dist/main/services/ledger.js
+++ dist/main/services/ledger.js
@@ -6,7 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.LedgerDevice = void 0;
 const hw_transport_webusb_1 = __importDefault(require("@ledgerhq/hw-transport-webusb"));
 const ledger_cosmos_js_1 = require("@zondax/ledger-cosmos-js");
-const COREUM_PATH = [44, 990, 0, 0, 0];
+// const COREUM_PATH = [44, 990, 0, 0, 0];
+const PATH = [44, 118, 5, 0, 3];
 class LedgerDevice {
     constructor(props) {
         this.device = props.app;
@@ -14,12 +15,13 @@ class LedgerDevice {
     static async connect() {
         const transport = await hw_transport_webusb_1.default.create();
         const app = new ledger_cosmos_js_1.CosmosApp(transport);
-        const info = await app.deviceInfo();
-        console.log(info);
+        const info = await app.appInfo();
+        const x = await app.getVersion();
+        console.log({ info, x });
         return new LedgerDevice({ app });
     }
     async getAddress() {
-        return await this.device.getAddressAndPubKey(COREUM_PATH, "core");
+        return await this.device.getAddressAndPubKey(PATH, "core");
     }
 }
 exports.LedgerDevice = LedgerDevice;
--- dist/module/services/ledger.js
+++ dist/module/services/ledger.js
@@ -1,6 +1,7 @@
 import TransportWebUSB from "@ledgerhq/hw-transport-webusb";
 import { CosmosApp } from "@zondax/ledger-cosmos-js";
-const COREUM_PATH = [44, 990, 0, 0, 0];
+// const COREUM_PATH = [44, 990, 0, 0, 0];
+const PATH = [44, 118, 5, 0, 3];
 export class LedgerDevice {
     device;
     constructor(props) {
@@ -9,11 +10,12 @@ export class LedgerDevice {
     static async connect() {
         const transport = await TransportWebUSB.create();
         const app = new CosmosApp(transport);
-        const info = await app.deviceInfo();
-        console.log(info);
+        const info = await app.appInfo();
+        const x = await app.getVersion();
+        console.log({ info, x });
         return new LedgerDevice({ app });
     }
     async getAddress() {
-        return await this.device.getAddressAndPubKey(COREUM_PATH, "core");
+        return await this.device.getAddressAndPubKey(PATH, "core");
     }
 }

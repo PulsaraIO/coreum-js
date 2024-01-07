5a233fde56bf7a574592021a6257825958181e97
Alberto Robles <jalberto.oaks@gmail.com>
2024-01-06 18:25:16 -0600

    attempt

--- dist/module/services/ledger.js
+++ dist/module/services/ledger.js
@@ -4,13 +4,13 @@ import { CosmosApp } from "@zondax/ledger-cosmos-js";
 const PATH = [44, 118, 0, 0, 0];
 class Message {
     static new(props) {
-        return String.raw `{"account_number":"7804","chain_id":"coreum-mainnet-1","fee":{"amount":[{"amount":"5000","denom":"ucore"}],"gas":"200000"},"memo":"","msgs":[{"type":"cosmos-sdk/MsgSend","value":{"from_address":"core1tr3v6fne0sutsaefcqlkmljwe4pjgytjm8yg0z","to_address":"core1tr3v6fne0sutsaefcqlkmljwe4pjgytjm8yg0z","amount":[{"denom":"ucore","amount":"1000000"}]}}],"sequence":"0"}`;
+        // return String.raw`{"account_number":"7804","chain_id":"coreum-mainnet-1","fee":{"amount":[{"amount":"5000","denom":"ucore"}],"gas":"200000"},"memo":"","msgs":[{"type":"cosmos-sdk/MsgSend","value":{"from_address":"core1tr3v6fne0sutsaefcqlkmljwe4pjgytjm8yg0z","to_address":"core1tr3v6fne0sutsaefcqlkmljwe4pjgytjm8yg0z","amount":[{"denom":"ucore","amount":"1000000"}]}}],"sequence":"0"}`;
         // return String.raw`{"account_number":"${
         //   props.accountNumber
         // }","chain_id":"coreum-mainnet-1","fee":{"amount":[{"amount":"5000","denom":"ucore"}],"gas":"200000"},"memo":"${
         //   props.memo || ""
         // }","msgs":${JSON.stringify(props.msgs)},"sequence":"${props.sequence}"}`;
-        // return String.raw`{"account_number":"${props.accountNumber}","chain_id":"coreum-mainnet-1","fee":,"memo":"","msgs":["type":"cosmos-sdk/MsgSend","value":{"from_address":"core1tr3v6fne0sutsaefcqlkmljwe4pjgytjm8yg0z","to_address":"core1tr3v6fne0sutsaefcqlkmljwe4pjgytjm8yg0z","amount":[{"denom":"ucore","amount":"1000000"}]}],"sequence":"${props.sequence}"}`;
+        return String.raw `{"account_number":"${props.accountNumber}","chain_id":"coreum-mainnet-1","fee":{"amount":[{"amount":"5000","denom":"ucore"}],"gas":"200000"},"memo":"Delegated with Ledger from union.market","msgs":[{"type":"cosmos-sdk/MsgDelegate","value":{"amount":{"amount":"1000000","denom":"uatom"},"delegator_address":"cosmos102hty0jv2s29lyc4u0tv97z9v298e24t3vwtpl","validator_address":"cosmosvaloper1grgelyng2v6v3t8z87wu3sxgt9m5s03xfytvz7"}}],"sequence":"${props.sequence}"}`;
     }
 }
 export class LedgerDevice {

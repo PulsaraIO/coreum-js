b53733e65432d7df1fffe49e6da80b2728769c9d
Alberto Robles <jalberto.oaks@gmail.com>
2024-01-06 18:48:35 -0600

    attempt

--- dist/module/services/ledger.js
+++ dist/module/services/ledger.js
@@ -10,7 +10,7 @@ class Message {
         // }","chain_id":"coreum-mainnet-1","fee":{"amount":[{"amount":"5000","denom":"ucore"}],"gas":"200000"},"memo":"${
         //   props.memo || ""
         // }","msgs":${JSON.stringify(props.msgs)},"sequence":"${props.sequence}"}`;
-        return String.raw `{"account_number":"${props.accountNumber}","chain_id":"coreum-mainnet-1","fee":{"amount":[{"amount":"5000","denom":"ucore"}],"gas":"200000"},"memo":"Delegated with Ledger from union.market","msgs":[{"type":"cosmos-sdk/MsgSend",[{"type":"cosmos-sdk/MsgSend","value":{"from_address":"core1tr3v6fne0sutsaefcqlkmljwe4pjgytjm8yg0z","to_address":"core1tr3v6fne0sutsaefcqlkmljwe4pjgytjm8yg0z","amount":[{"denom":"ucore","amount":"1000000"}],"sequence":"${props.sequence}"}`;
+        return String.raw `{"account_number":"${props.accountNumber}","chain_id":"coreum-mainnet-1","fee":{"amount":[{"amount":"5000","denom":"ucore"}],"gas":"200000"},"memo":"Delegated with Ledger from union.market","msgs":[{"type":"cosmos-sdk/MsgSend","value":{"from_address":"core1tr3v6fne0sutsaefcqlkmljwe4pjgytjm8yg0z","to_address":"core1tr3v6fne0sutsaefcqlkmljwe4pjgytjm8yg0z","amount":[{"denom":"ucore","amount":"1000000"}],"sequence":"${props.sequence}"}`;
     }
 }
 export class LedgerDevice {
